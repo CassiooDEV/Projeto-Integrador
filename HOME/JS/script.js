6d262c6361e84e8761d03a6ad19e0b7a755982a4
samupapati <samuelpapati@gmail.com>
2022-06-27 21:30:58 -0300

    menu

--- HOME/JS/script.js
+++ HOME/JS/script.js
@@ -68,5 +68,20 @@ function verifState() {
 //     }
 // }
 
+const botao = document.getElementById('b');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active')
+}
+
+btn.addEventListener('click', toggleMenu);
+
+function ChangeBG(){
+    document.getElementById('btn').classList.contains("alt") ? document.getElementById('btn').classList.remove("alt") : document.getElementById('btn').classList.add("alt");
+}
+
+
+// Fim do header
 
 
