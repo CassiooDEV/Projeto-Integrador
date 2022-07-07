a01a19999ba3f63d59dffe395e5615f29f821297
herculeslbs <herculeslbs@gmail.com>
2022-07-06 21:49:11 -0300

    subindo atualização

--- Contato/script.js
+++ Contato/script.js
@@ -1,4 +1,11 @@
+let fecharform = document.getElementById('fecharform');
+let mostrar = document.getElementsByClassName('mostrar');
+let formulario = document.getElementById('formulario');
 let buttons = document.getElementsByClassName('botaoForm');
 Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', alert())
+    button.addEventListener('click', mostraformulario)
 })
+fecharform.addEventListener('click', mostraformulario)
+function mostraformulario(){
+   formulario.classList.toggle("mostrar")
+}
